335b5f4b5d892f4092f1baceff63fe7e3d73d71d
michelleap <maperetz@berkeley.edu>
2017-10-08 20:27:32 -0700

    csv files and scoring example

--- Initial+Exploration_scoring.py
+++ Initial+Exploration_scoring.py
@@ -7,7 +7,7 @@ import numpy as np
 import pandas as pd
 import random
 from sklearn.linear_model import LogisticRegression
-df = pd.read_csv('ConsolidatedSchools')
+df = pd.read_csv('ConsolidatedSchools.csv')
 
 
 # In[4]:
