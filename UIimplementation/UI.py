0e40e475476540a9a6acd688727fda4a1528466a
Conner <conner.hunihan@gmail.com>
2017-10-14 18:15:28 -0700

    reverted to tk styling (StringVar doesn't exist in ttk), state/match values now pass to respective dictionaries

--- UIimplementation/UI.py
+++ UIimplementation/UI.py
@@ -1,25 +1,28 @@
 import tkinter as tk
-from tkinter import ttk
 from functions import *
 """
-STILL NEED TO FIGURE OUT:
-1. How to pass state & match choices from UI into the following:
-state_choice = (input('Please select which states you are interested in exploring (OR, WA, UT, etc.) Please separate by commas, or enter "all" for all states: ')).upper()
-match_level = int(input('Please select matching level 0 to 4 (0=loose 4=strict): ')) 
+GetSchooled is the final project for Info206: Software Prototyping. It was created
+by a group of graduate students at Berkeley's School of Information including:
+	Alyssa Li
+	Jake Mainwaring
+	Michelle Peretz
+	Conner Hunihan (www.connerhunihan.com)
 
-2. Can you explain how the GetSchooled class works?
-3. Format – replacing pack() w/ grid()
-4. Why is stateEntered not being read as defined?
+The program is designed to help students find colleges.  
+
+TO DO:
+1. Format – replacing pack() w/ grid()
 """
+
+# If testing, change flag to True 
+testing = True
 	
 LARGE_FONT = ("Verdana", 21)
 SECOND_FONT = ("Verdana", 12)
-buttonChoices = []
-list_of_answers = []
-# row_number = 0
 
-# Globally defined to be able to pass back/forth btwn UI and data layer
-collegeList = []
+list_of_answers = []
+states = []
+match = []
 
 def record_button_1(*args):
 	record_selection(1)
@@ -28,221 +31,267 @@ def record_button_2(*args):
 def record_button_3(*args):
 	record_selection(3)
 
-# def submit_entries(*args):
-# 	print(StartPage.entryState.get())
-# 	print(StartPage.entryMatch.get())
-
 def record_selection (scenario_choice):    
     list_of_answers.append(scenario_choice)
-    print(list_of_answers)
+    if testing == True:
+    	print("List of college selections: ", list_of_answers)
+    	print("Number of clicks: ", len(list_of_answers))
 
 def erase_selections (*args):
 	list_of_answers.clear()
-
-def return_state_and_match (*args):
-	state = stateEntered.get()
-	match = entryMatch.get()
-	print(state, match)
+	states.clear()
+	match.clear()
 
 class GetSchooled(tk.Tk):
-	"""Sets up the UI for the program"""
+	'''sets up the application, creates single page frame'''
 	def __init__(self, *args, **kwargs):
 		tk.Tk.__init__(self, *args, **kwargs)
-		# tk.Tk.iconbitmap(self, default="GetSchooled_icon.ico")
 		tk.Tk.wm_title(self, "GetSchooled")
-		container = ttk.Frame(self)
+		container = tk.Frame(self)
 		container.pack(side="top", fill="both", expand=True)
 		container.grid_rowconfigure(0, weight=1)	#0 sets minimum size, weight=1 is a priority thing
 		container.grid_columnconfigure(0, weight=1)
 		
 		self.frames = {}
 		
-		for F in (StartPage, PageOne, PageTwo, PageThree, PageFour, PageFive):
-
+		# generate pages from dictionary 
+		for F in (StartPage, PageOne, PageTwo, PageThree, PageFour, PageFive, ResultsPage, DisplayPage):
 			frame = F(container, self)
-			
 			self.frames[F] = frame
-			
 			frame.grid(row=0, column=0, sticky="nsew")
 		
 		self.show_frame(StartPage)
 
 	def show_frame(self, cont):	#cont dictates which key number frame to show on front
+		'''raises frame to active view-level'''
 		frame = self.frames[cont]
 		frame.tkraise()		#raises frame to the front
 
-class StartPage(ttk.Frame):
-	def __init__(self, parent, controller):
-		ttk.Frame.__init__(self, parent)		#parent class is GetSchooled
-		label = ttk.Label(self, text="GetSchooled", font=LARGE_FONT)
+	def GetEntries(self, StartPage):
+		'''record state(s) and matches user has entered'''
+		states.append(StartPage.stateEntered.get())
+		match.append(StartPage.matchEntered.get())
+		if testing == True:
+			print("States entered: ", states)
+			print("Match level entered: ", match)
+
+class StartPage(tk.Frame):
+	def __init__(self, parent, controller):	
+		'''parent references the container parameter in F()...line61.  controller references the self in F()...line61'''
+		tk.Frame.__init__(self, parent) #parent parameter references GetSchooled
+		label = tk.Label(self, text="GetSchooled", font=LARGE_FONT)
 		label.pack(pady=5, padx=5)
 
-		introText = ttk.Label(self, text="THIS IS WHERE THE PROGRAM WILL GO", font=SECOND_FONT)
+		# instantiate Label widget
+		introText = tk.Label(self, text="THIS IS WHERE THE PROGRAM WILL GO", font=SECOND_FONT)
+		# make Label widget appear with .pack()
 		introText.pack(pady=10, padx=10)
 
-		#ENTRY FRAME
-		# entryFrame = ttk.Frame(self).grid(row=0, column=0)
-		entryState_label = ttk.Label(self, text="Enter which states you are interested in").pack()
-		stateEntered = tk.StringVar()
-		entryState = ttk.Entry(self, textvariable=stateEntered).pack()
+		# create label and state entry widgets, make both appear w pack() function
+		self.stateEntered = tk.StringVar()
+		entryState_label = tk.Label(self, text="Please select which states you are interested in exploring (OR, WA, UT, etc.) \nPlease separate by commas, or enter \"all\" for all states: ").pack()
+		entryState = tk.Entry(self, textvariable=self.stateEntered).pack()
 
-		entryMatch_label = ttk.Label(self, text="Enter how strict a match").pack()
-		matchEntered = tk.StringVar()
-		entryMatch = ttk.Entry(self, textvariable=matchEntered).pack()
-	
-		submitButton = ttk.Button(self, text="Click to submit", 
-			command=lambda: controller.show_frame(PageOne))
+		# create label and match entry widgets, make both appear w pack() function
+		self.matchEntered = tk.StringVar()
+		entryMatch_label = tk.Label(self, text="Please select matching level: \n0 to 4 (0=loose 4=strict): ").pack()
+		entryMatch = tk.Entry(self, textvariable=self.matchEntered).pack()
+		
+		# create submit button that calls submit_entries function, when pressed
+		submitButton = tk.Button(self, text="Click to submit", 
+			command=lambda: self.submit_entries(PageOne, controller))
 		submitButton.pack()
-		submitButton.bind("<1>", return_state_and_match)
 
-	def return_state_and_match (self):
-		state = stateEntered.get()
-		match = entryMatch.get()
-		print(state, match)
+	def submit_entries (self, PageOne, app):
+		'''app.GetEntries adds values to their respective lists; show_frame navigates to PageOne screen'''
+		app.GetEntries(self)
+		app.show_frame(PageOne)
 
-
-class PageOne(ttk.Frame):
+class PageOne(tk.Frame):
+	'''this class instantiates as a whole-window frame'''
 	def __init__(self, parent, controller):
-		ttk.Frame.__init__(self, parent)
-		label = ttk.Label(self, text="Page One", font=LARGE_FONT)
+		tk.Frame.__init__(self, parent)
+		label = tk.Label(self, text="Page One", font=LARGE_FONT)
 		label.pack(pady=10, padx=10)
 
-		#PLACEHODLER FOR...STATE AND MATCH SUBMITTED ARE DEFAULT VALUES, STATE=DISABLED
-
+		# pulls hypothetical colleges as a string returned from college_selection function
 		colleges = tk.StringVar()
 		colleges.set(college_selection(0))
-		display = ttk.Label(self, textvariable=colleges)
+		display = tk.Label(self, textvariable=colleges)
 		display.pack()
 
-		button1 = ttk.Button(self, text="Select College 1", command=lambda: controller.show_frame(PageTwo))
+		# button click navigates to next page, button.bind handler records the button click value
+		button1 = tk.Button(self, text="Select College 1", command=lambda: controller.show_frame(PageTwo))
 		button1.pack()
 		button1.bind("<1>", record_button_1)
-		button1 = ttk.Button(self, text="Select College 2", command=lambda: controller.show_frame(PageTwo))
+		button1 = tk.Button(self, text="Select College 2", command=lambda: controller.show_frame(PageTwo))
 		button1.pack()
 		button1.bind("<1>", record_button_2)
-		button1 = ttk.Button(self, text="Select College 3", command=lambda: controller.show_frame(PageTwo))
+		button1 = tk.Button(self, text="Select College 3", command=lambda: controller.show_frame(PageTwo))
 		button1.pack()
 		button1.bind("<1>", record_button_3)
 
-		resetButton = ttk.Button(self, text="RESET PROGRAM", 
+		resetButton = tk.Button(self, text="RESET PROGRAM", 
 			command=lambda: controller.show_frame(StartPage))
 		resetButton.pack(pady=25)
 		resetButton.bind("<1>", erase_selections)
 
-class PageTwo(ttk.Frame):
+class PageTwo(tk.Frame):
+	'''this class instantiates as a whole-window frame'''
 	def __init__(self, parent, controller):
-		ttk.Frame.__init__(self, parent)
-		label = ttk.Label(self, text="Page Two", font=LARGE_FONT)
+		tk.Frame.__init__(self, parent)
+		label = tk.Label(self, text="Page Two", font=LARGE_FONT)
 		label.pack(pady=10, padx=10)
 
-		#PLACEHODLER FOR...STATE AND MATCH SUBMITTED ARE DEFAULT VALUES, STATE=DISABLED
-
+		# pulls hypothetical colleges as a string returned from college_selection function
 		colleges = tk.StringVar()
-		colleges.set(college_selection(1))
-		display = ttk.Label(self, textvariable=colleges)
+		colleges.set(college_selection(0))
+		display = tk.Label(self, textvariable=colleges)
 		display.pack()
 
-		button1 = ttk.Button(self, text="Select College 1", command=lambda: controller.show_frame(PageThree))
+		# button click navigates to next page, button.bind handler records the button click value
+		button1 = tk.Button(self, text="Select College 1", command=lambda: controller.show_frame(PageThree))
 		button1.pack()
 		button1.bind("<1>", record_button_1)
-		button1 = ttk.Button(self, text="Select College 2", command=lambda: controller.show_frame(PageThree))
+		button1 = tk.Button(self, text="Select College 2", command=lambda: controller.show_frame(PageThree))
 		button1.pack()
 		button1.bind("<1>", record_button_2)
-		button1 = ttk.Button(self, text="Select College 3", command=lambda: controller.show_frame(PageThree))
+		button1 = tk.Button(self, text="Select College 3", command=lambda: controller.show_frame(PageThree))
 		button1.pack()
 		button1.bind("<1>", record_button_3)
 
-		resetButton = ttk.Button(self, text="RESET PROGRAM", 
+		resetButton = tk.Button(self, text="RESET PROGRAM", 
 			command=lambda: controller.show_frame(StartPage))
 		resetButton.pack(pady=25)
 		resetButton.bind("<1>", erase_selections)
 
 
-class PageThree(ttk.Frame):
+class PageThree(tk.Frame):
+	'''this class instantiates as a whole-window frame'''
 	def __init__(self, parent, controller):
-		ttk.Frame.__init__(self, parent)
-		label = ttk.Label(self, text="Page Three", font=LARGE_FONT)
+		tk.Frame.__init__(self, parent)
+		label = tk.Label(self, text="Page Three", font=LARGE_FONT)
 		label.pack(pady=10, padx=10)
 
-		#PLACEHODLER FOR...STATE AND MATCH SUBMITTED ARE DEFAULT VALUES, STATE=DISABLED
-
+		# pulls hypothetical colleges as a string returned from college_selection function
 		colleges = tk.StringVar()
-		colleges.set(college_selection(2))
-		display = ttk.Label(self, textvariable=colleges)
+		colleges.set(college_selection(0))
+		display = tk.Label(self, textvariable=colleges)
 		display.pack()
 
-		button1 = ttk.Button(self, text="Select College 1", command=lambda: controller.show_frame(PageFour))
+		# button click navigates to next page, button.bind handler records the button click value
+		button1 = tk.Button(self, text="Select College 1", command=lambda: controller.show_frame(PageFour))
 		button1.pack()
 		button1.bind("<1>", record_button_1)
-		button1 = ttk.Button(self, text="Select College 2", command=lambda: controller.show_frame(PageFour))
+		button1 = tk.Button(self, text="Select College 2", command=lambda: controller.show_frame(PageFour))
 		button1.pack()
 		button1.bind("<1>", record_button_2)
-		button1 = ttk.Button(self, text="Select College 3", command=lambda: controller.show_frame(PageFour))
+		button1 = tk.Button(self, text="Select College 3", command=lambda: controller.show_frame(PageFour))
 		button1.pack()
 		button1.bind("<1>", record_button_3)
 
-		resetButton = ttk.Button(self, text="RESET PROGRAM", 
+		resetButton = tk.Button(self, text="RESET PROGRAM", 
 			command=lambda: controller.show_frame(StartPage))
 		resetButton.pack(pady=25)
 		resetButton.bind("<1>", erase_selections)
 		
 
-class PageFour(ttk.Frame):
+class PageFour(tk.Frame):
+	'''this class instantiates as a whole-window frame'''
 	def __init__(self, parent, controller):
-		ttk.Frame.__init__(self, parent)
-		label = ttk.Label(self, text="Page Four", font=LARGE_FONT)
+		tk.Frame.__init__(self, parent)
+		label = tk.Label(self, text="Page Four", font=LARGE_FONT)
 		label.pack(pady=10, padx=10)
 
-		#PLACEHODLER FOR...STATE AND MATCH SUBMITTED ARE DEFAULT VALUES, STATE=DISABLED
-
+		# pulls hypothetical colleges as a string returned from college_selection function
 		colleges = tk.StringVar()
-		colleges.set(college_selection(3))
-		display = ttk.Label(self, textvariable=colleges)
+		colleges.set(college_selection(0))
+		display = tk.Label(self, textvariable=colleges)
 		display.pack()
 
-		button1 = ttk.Button(self, text="Select College 1", command=lambda: controller.show_frame(PageFive))
+		# button click navigates to next page, button.bind handler records the button click value
+		button1 = tk.Button(self, text="Select College 1", command=lambda: controller.show_frame(PageFive))
 		button1.pack()
 		button1.bind("<1>", record_button_1)
-		button1 = ttk.Button(self, text="Select College 2", command=lambda: controller.show_frame(PageFive))
+		button1 = tk.Button(self, text="Select College 2", command=lambda: controller.show_frame(PageFive))
 		button1.pack()
 		button1.bind("<1>", record_button_2)
-		button1 = ttk.Button(self, text="Select College 3", command=lambda: controller.show_frame(PageFive))
+		button1 = tk.Button(self, text="Select College 3", command=lambda: controller.show_frame(PageFive))
 		button1.pack()
 		button1.bind("<1>", record_button_3)
 
-		resetButton = ttk.Button(self, text="RESET PROGRAM", 
+		resetButton = tk.Button(self, text="RESET PROGRAM", 
 			command=lambda: controller.show_frame(StartPage))
 		resetButton.pack(pady=25)
 		resetButton.bind("<1>", erase_selections)
 
-class PageFive(ttk.Frame):
+class PageFive(tk.Frame):
+	'''this class instantiates as a whole-window frame'''
 	def __init__(self, parent, controller):
-		ttk.Frame.__init__(self, parent)
-		label = ttk.Label(self, text="Page Five", font=LARGE_FONT)
+		tk.Frame.__init__(self, parent)
+		label = tk.Label(self, text="PageFive", font=LARGE_FONT)
 		label.pack(pady=10, padx=10)
 
-		#PLACEHODLER FOR...STATE AND MATCH SUBMITTED ARE DEFAULT VALUES, STATE=DISABLED
-
+		# pulls hypothetical colleges as a string returned from college_selection function
 		colleges = tk.StringVar()
-		colleges.set(college_selection(4))
-		display = ttk.Label(self, textvariable=colleges)
+		colleges.set(college_selection(0))
+		display = tk.Label(self, textvariable=colleges)
 		display.pack()
 
-		button1 = ttk.Button(self, text="Select College 1", command=lambda: controller.show_frame(PageFive))
+		# button click navigates to next page, button.bind handler records the button click value
+		button1 = tk.Button(self, text="Select College 1", command=lambda: controller.show_frame(ResultsPage))
 		button1.pack()
 		button1.bind("<1>", record_button_1)
-		button1 = ttk.Button(self, text="Select College 2", command=lambda: controller.show_frame(PageFive))
+		button1 = tk.Button(self, text="Select College 2", command=lambda: controller.show_frame(ResultsPage))
 		button1.pack()
 		button1.bind("<1>", record_button_2)
-		button1 = ttk.Button(self, text="Select College 3", command=lambda: controller.show_frame(PageFive))
+		button1 = tk.Button(self, text="Select College 3", command=lambda: controller.show_frame(ResultsPage))
 		button1.pack()
 		button1.bind("<1>", record_button_3)
 
-		resetButton = ttk.Button(self, text="RESET PROGRAM", 
+		resetButton = tk.Button(self, text="RESET PROGRAM", 
 			command=lambda: controller.show_frame(StartPage))
 		resetButton.pack(pady=25)
 		resetButton.bind("<1>", erase_selections)
 
+class ResultsPage(tk.Frame):
+	'''this class instantiates as a whole-window frame'''
+	def __init__(self, parent, controller):
+		tk.Frame.__init__(self, parent)
+		label = tk.Label(self, text="RESULTS", font=LARGE_FONT)
+		label.pack(pady=10, padx=10)
+
+		# pulls hypothetical colleges as a string returned from college_selection function
+		results = tk.StringVar()
+		results.set('''function to display scores''')
+		display = tk.Label(self, textvariable=results)
+		display.pack()
+
+		label = tk.Label(self, text="Do you want more information on these schools?", font=SECOND_FONT)
+		label.pack(pady=10, padx=10)
+
+		displayButton = tk.Button(self, text="SHOW ME", 
+			command=lambda: controller.show_frame(DisplayPage))
+		displayButton.pack(pady=25)
+		displayButton.bind("<1>", erase_selections)
+
+class DisplayPage(tk.Frame):
+	'''this class instantiates as a whole-window frame'''
+	def __init__(self, parent, controller):
+		tk.Frame.__init__(self, parent)
+		label = tk.Label(self, text="RESULTS", font=LARGE_FONT)
+		label.pack(pady=10, padx=10)
+
+		# pulls hypothetical colleges as a string returned from college_selection function
+		results = tk.StringVar()
+		results.set('''function to display scores''')
+		display = tk.Label(self, textvariable=results)
+		display.pack()
+
+		startOverButton = tk.Button(self, text="START OVER", 
+			command=lambda: controller.show_frame(StartPage))
+		startOverButton.pack(pady=25)
+		startOverButton.bind("<1>", erase_selections)
 
 app = GetSchooled()
 app.mainloop()
